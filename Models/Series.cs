88f4a2b2d650a198ed06e52f643fb9aa46e02978
ShreyasVH <shreyas.deve.1@gmail.com>
2023-11-04 16:04:08 +0530

    updated series name length

--- Models/Series.cs
+++ Models/Series.cs
@@ -11,7 +11,7 @@ namespace Com.Dotnet.Cric.Models
     {
         public long Id { get; set; }
         [Required]
-        [MaxLength(50)]
+        [MaxLength(100)]
         public string Name { get; set; }
         
         [Required]
