448b8a840314591fef2add25cd08d44edd693b4d
ShreyasVH <shreyas.deve.1@gmail.com>
2025-03-19 11:55:14 +0530

    added new result types

---
+++ Migrations/20250319060913_NewResultTypes.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewResultTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ResultTypes",
+                columns: new[] { "Name" },
+                values: new object[,]
+                {
+                    { "Least Wickets" },
+                    { "Boundary Count" }
+                }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM ResultTypes WHERE Name IN ('Least Wickets', 'Boundary Count')");
+        }
+    }
+}
