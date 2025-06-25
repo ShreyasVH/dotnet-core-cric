78456b6c43ab2bdbb132788b92463de1f9bca0b8
ShreyasVH <shreyas.deve.1@gmail.com>
2025-06-26 00:36:23 +0530

    added city in stadium uniqueness

---
+++ Migrations/20250625141128_UpdateStadiumUniqueKey.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateStadiumUniqueKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Stadiums_Name_CountryId",
+                table: "Stadiums"
+            );
+            
+            migrationBuilder.CreateIndex(
+                name: "IX_Stadiums_Name_CountryId_City",
+                table: "Stadiums",
+                columns: new[] { "Name", "CountryId", "City" },
+                unique: true
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Stadiums_Name_CountryId_City",
+                table: "Stadiums"
+            );
+            
+            migrationBuilder.CreateIndex(
+                name: "IX_Stadiums_Name_CountryId",
+                table: "Stadiums",
+                columns: new[] { "Name", "CountryId" },
+                unique: true
+            );
+        }
+    }
+}
