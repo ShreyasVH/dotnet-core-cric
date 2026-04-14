6b215b758424b948c8b1682403fd3f02faf2f165
ShreyasVH <shreyas.deve.1@gmail.com>
2026-04-14 07:26:06 +0530

    using same query method

--- Repositories/CustomRepository.cs
+++ Repositories/CustomRepository.cs
@@ -20,30 +20,6 @@ namespace Com.Dotnet.Cric.Repositories
             _dbContext = dbContext;
         }
 
-        public List<Dictionary<string, object>> ExecuteQuery(string query)
-        {
-            var result = new List<Dictionary<string, object>>();
-            using var command = _dbContext.Database.GetDbConnection().CreateCommand();
-            command.CommandText = query;
-            _dbContext.Database.OpenConnection();
-            
-            using var reader = command.ExecuteReader();
-
-            while (reader.Read())
-            {
-                var row = new Dictionary<string, object>();
-                for (var i = 0; i < reader.FieldCount; i++)
-                {
-                    var columnName = reader.GetName(i);
-                    var columnValue = reader.GetValue(i);
-                    row[columnName] = columnValue;
-                }
-                result.Add(row);
-            }
-
-            return result;
-        }
-        
         public List<Dictionary<string, object>> ExecuteQuery(
             string query,
             Dictionary<string, object>? parameters = null)
